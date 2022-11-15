2567b83b8012561f38c933c0c26761df997bdf9c
David Vasquez <99619761+dvasquez08@users.noreply.github.com>
2022-11-15 06:18:59 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,8 @@ class Attendance extends StatefulWidget {
 }
 
 class _AttendanceState extends State<Attendance> {
-  String studentCode = " ";
   String scanResult = " ";
+  String code = " ";
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +64,7 @@ class _AttendanceState extends State<Attendance> {
               SizedBox(height: 15.0),
               // The text that appears after scanning a barcode
               Text(
-                scanResult == null ? "Scan a code!" : "Hello $scanResult!",
+                scanResult == null ? "Scan a code!" : "Thank you for checking in!",
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               )
             ],
@@ -89,11 +89,19 @@ class _AttendanceState extends State<Attendance> {
     if (!mounted) return;
 
     setState(() => this.scanResult = scanResult);
-
+    
+    // Telling the app what data to save
     Map<String, dynamic> dataToSend = {
-      'code': scanResult,
+      'timestamp': DateTime.now(),
     };
+    
+    // Code that sends the data to Firestore using the saved data
+    code = scanResult;
 
-    FirebaseFirestore.instance.collection('students').add(dataToSend);
+    FirebaseFirestore.instance
+        .collection('students')
+        .doc(code)
+        .collection('Attendance')
+        .add(dataToSend);
   }
 }
