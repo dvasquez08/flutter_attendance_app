4d62f54ff2513519927f5cb9f00a331a5d180373
David Vasquez <99619761+dvasquez08@users.noreply.github.com>
2022-11-27 22:21:15 -0700

    Update main.dart
    Updated code with the fancier button, background image and the drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:attendance/add_north.dart';
+import 'package:attendance/location_selection.dart';
+import 'package:attendance/ready_testing_north.dart';
+import 'package:attendance/testing_north.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'components.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(Attendance());
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Attendance()));
 }
 
 class Attendance extends StatefulWidget {
@@ -20,55 +25,138 @@ class Attendance extends StatefulWidget {
 }
 
 class _AttendanceState extends State<Attendance> {
+  String timestamp = " ";
   String scanResult = " ";
   String code = " ";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.indigo,
-          title: Text("Taegeuk Taekwondo Attendance",
-              style: GoogleFonts.openSans()),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/new-logo.png"),
-              SizedBox(height: 15.0),
-              SansText(
-                "Welcome to Taegeuk Taekwondo!",
-                40.0,
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              SansText("Please scan your ID card to check-in:", 20.0),
-              SizedBox(height: 15.0),
-
-              // section for the button to press for scanning
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.black,
-                ),
-                icon: Icon(Icons.camera_alt_outlined),
-                label: Text("Start Scan"),
-                onPressed: scanBarcode,
-              ),
-              SizedBox(height: 15.0),
-              // The text that appears after scanning a barcode
-              Text(
-                scanResult == null ? "Scan a code!" : "Thank you for checking in!",
-                style: TextStyle(fontSize: 25.0, color: Colors.white),
-              )
-            ],
+    var heightDevice = MediaQuery.of(context).size.height;
+    var widthDevice = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        title:
+            Text("Taegeuk Taekwondo Attendance", style: GoogleFonts.openSans()),
+        centerTitle: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/class3.png"),
+            fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/new-logo.png"),
+            SizedBox(height: 15.0),
+            SansText(
+              "Welcome to Taegeuk Taekwondo!",
+              50.0,
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            SansText("Please scan your ID card to check-in:", 25.0),
+            SizedBox(height: 15.0),
+
+            // section for the button to press for scanning
+
+            GestureDetector(
+              onTap: scanBarcode,
+              child: Container(
+                height: widthDevice / 17,
+                width: heightDevice / 10,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.indigo,
+                      offset: Offset(2, 2),
+                      blurRadius: 25,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [Image.asset("assets/barscan.png")],
+                ),
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Text(
+              scanResult == null ? "Scan a code!" : "Thank you",
+              style: TextStyle(fontSize: 25.0, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+                decoration: BoxDecoration(color: Colors.red),
+                child: SansText('Menu', 30.0)),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => readyTestNorth(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Ready for Testing",
+                    style: TextStyle(fontSize: 20.0, color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                MaterialButton(
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => testingNorth(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Testing Check-in",
+                    style: TextStyle(fontSize: 20.0, color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                MaterialButton(
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => location_selection(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Select Another Location",
+                    style: TextStyle(fontSize: 20.0, color: Colors.black),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
@@ -89,13 +177,14 @@ class _AttendanceState extends State<Attendance> {
     if (!mounted) return;
 
     setState(() => this.scanResult = scanResult);
-    
-    // Telling the app what data to save
+
+    // Telling the app what data to save which is the timestamp
     Map<String, dynamic> dataToSend = {
       'timestamp': DateTime.now(),
     };
-    
-    // Code that sends the data to Firestore using the saved data
+
+    // This part sends the timestamp of the scanned barcode and sends it to
+    // Firestore.
     code = scanResult;
 
     FirebaseFirestore.instance
