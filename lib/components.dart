fc8d64b3f2259b40ecdb949f4a0278139ff864e7
David Vasquez <99619761+dvasquez08@users.noreply.github.com>
2022-12-10 02:53:28 -0700

    Update components.dart
    Added another text widget and a widget for buttons which will save lots of time for future pages

--- lib/components.dart
+++ lib/components.dart
@@ -17,3 +17,44 @@ class SansText extends StatelessWidget {
     );
   }
 }
+
+class TextBlack extends StatelessWidget {
+  final text;
+  final size;
+  const TextBlack(this.text, this.size, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: GoogleFonts.openSans(
+          fontSize: size, fontWeight: FontWeight.w300, color: Colors.black),
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final text;
+  final page;
+  const NavButton(this.text, this.page, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      shape: Border.all(color: Colors.black),
+      color: Colors.white,
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => page,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 20.0, color: Colors.black),
+      ),
+    );
+  }
+}
