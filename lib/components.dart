31eadb527a82191fa4a4b9cdef76e4a2324d1ab4
David Vasquez <99619761+dvasquez08@users.noreply.github.com>
2022-12-18 20:55:46 -0700

    Update components.dart
    Updated the component files with new widgets that were made

--- lib/components.dart
+++ lib/components.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+// This file contains all the widgets that the other pages call.
+// These widgets are for commonly used code such as text sections
+// that use the same font, buttons that look the same on multiple
+// pages, etc.
 
 class SansText extends StatelessWidget {
   final text;
@@ -56,5 +61,59 @@ class NavButton extends StatelessWidget {
         style: TextStyle(fontSize: 20.0, color: Colors.black),
       ),
     );
+  }
+}
+
+class ScanButton extends StatefulWidget {
+  const ScanButton({Key? key}) : super(key: key);
+
+  @override
+  State<ScanButton> createState() => _ScanButtonState();
+}
+
+class _ScanButtonState extends State<ScanButton> {
+  String scanResult = " ";
+  @override
+  Widget build(BuildContext context) {
+    var heightDevice = MediaQuery.of(context).size.height;
+    var widthDevice = MediaQuery.of(context).size.width;
+    return GestureDetector(
+      onTap: scanBarcode,
+      child: Container(
+        height: widthDevice / 17,
+        width: heightDevice / 10,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.indigo,
+              offset: Offset(2, 2),
+              blurRadius: 25,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Image.asset("assets/barscan.png")],
+        ),
+      ),
+    );
+  }
+  Future scanBarcode() async {
+    try {
+      scanResult = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6677",
+        "Cancel",
+        true,
+        ScanMode.BARCODE,
+      );
+    } on PlatformException {
+      scanResult = "Failed to get platform version.";
+    }
+    if (!mounted) return;
+
+    setState(() => this.scanResult = scanResult);
   }
 }
