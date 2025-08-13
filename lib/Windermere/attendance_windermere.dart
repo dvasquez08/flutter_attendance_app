4572c82e1bd7fa2ddf1edcb314f6b4781218e0be
David Vasquez <99619761+dvasquez08@users.noreply.github.com>
2025-08-12 22:42:17 -0600

    Updated comments to explain code

--- lib/Windermere/attendance_windermere.dart
+++ lib/Windermere/attendance_windermere.dart
@@ -1,14 +1,20 @@
+// Imports of other pages for the Windermere location and to the main page
 import 'package:attendance/Windermere/ready_testing_windermere.dart';
 import 'package:attendance/Windermere/testing_windermere.dart';
 import 'package:attendance/location_selection.dart';
+// Firebase imports
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+// Flutter imports
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+// Package imports such as Google fonts and the barcode scanner package
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:firebase_core/firebase_core.dart';
+// Importing reusable code from the components dart file
 import '../components.dart';
 
+// Initializing Firebase. This function is async because Firebase initialization must be completed before the app runs
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -16,6 +22,7 @@ Future<void> main() async {
       debugShowCheckedModeBanner: false, home: attendanceWindermere()));
 }
 
+// A StatefulWidget that manages the attendance check-in process for the Terwilliger location.
 class attendanceWindermere extends StatefulWidget {
   const attendanceWindermere({Key? key}) : super(key: key);
 
@@ -24,6 +31,7 @@ class attendanceWindermere extends StatefulWidget {
 }
 
 class _attendanceWindermereState extends State<attendanceWindermere> {
+  // State variables to hold the timestamp, the result of the barcode scan, and the student's code.
   String timestamp = " ";
   String scanResult = " ";
   String code = " ";
@@ -31,9 +39,13 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
 
   @override
   Widget build(BuildContext context) {
+    // These variables capture the device's screen dimensions.
+    // They are used here for adjusting the size of containers and adding responsiveness.
     var heightDevice = MediaQuery.of(context).size.height;
     var widthDevice = MediaQuery.of(context).size.width;
     return Scaffold(
+      
+      //App bar section that contains the screen title
       backgroundColor: Colors.transparent,
       appBar: AppBar(
         backgroundColor: Colors.black26,
@@ -41,6 +53,8 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
             style: GoogleFonts.openSans(fontWeight: FontWeight.w300)),
         centerTitle: true,
       ),
+
+      // Main content of the screen, also sets the background image
       body: Container(
         alignment: Alignment.center,
         decoration: const BoxDecoration(
@@ -49,8 +63,12 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
             fit: BoxFit.cover,
           ),
         ),
+
+        // Column arranges the content in a vertical array.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+
+          // Welcome section that contains the logo and directs the user to scan their barcode
           children: [
             Image.asset("assets/new-logo.png"),
             SizedBox(height: 15.0),
@@ -59,11 +77,12 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
             SansText("Please scan your ID card to check-in:", 25.0),
             SizedBox(height: 15.0),
 
-            // Here, I made a widget called ScanButton which takes the Gesture
-            // detector used from the North pages and condenses it to just one
-            // line of code here. This too is added in components.
+            // Pulling the ScanButton component from the components file. This takes
+            // The Gesture detector used in the North pages and condenses it to just
+            // this single line here.
             ScanButton(),
             SizedBox(height: 15.0),
+            // What will appear when scanning the barcode
             Text(
               scanResult == null ? "Scan a code!" : "Thank you",
               style: TextStyle(fontSize: 25.0, color: Colors.white),
@@ -71,10 +90,13 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
           ],
         ),
       ),
+
+      // Drawer for the slide-out menu for navigation.
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
+            // The header for the drawer.
             const DrawerHeader(
                 decoration: BoxDecoration(color: Colors.red),
                 child: SansText('Menu', 30.0)),
@@ -84,11 +106,11 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
               // and more readable. All widgets made are found in components.dart
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                NavButton("Ready for Testing", readyTestWindermere()),
+                NavButton("Ready for Testing", readyTestWindermere()), // The page to add students to the list that are ready for testing
                 SizedBox(height: 15.0),
-                NavButton("Testing Check-in", testingWindermere()),
+                NavButton("Testing Check-in", testingWindermere()), // To the attendance page for testing day
                 SizedBox(height: 15.0),
-                NavButton("Select Another Location", location_selection())
+                NavButton("Select Another Location", location_selection()) // Takes user to app's main location selector page
               ],
             )
           ],
@@ -101,33 +123,36 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
   Future scanBarcode() async {
     try {
       scanResult = await FlutterBarcodeScanner.scanBarcode(
-        "#ff6677",
-        "Cancel",
-        true,
-        ScanMode.BARCODE,
+        "#ff6677", // Sets the color of the line across the scanner
+        "Cancel", // Adds the cancel button
+        true, // Boolean value to enable the flash
+        ScanMode.BARCODE, // Set  to Barcode mode, other option is QR mode for QR scanner
       );
     } on PlatformException {
-      scanResult = "Failed to get platform version.";
+      scanResult = "Failed to get platform version."; // Error handling
     }
+    // A check to ensure the widget is still in the widget tree before updating state.
+    // This prevents errors if the user navigates away while scanning is in progress.
     if (!mounted) return;
 
-    setState(() => this.scanResult = scanResult);
+    setState(() => this.scanResult = scanResult); // Update the state with the result of the scan
 
     // Telling the app what data to save. This is saving the timestamp and the code
     // which sends data to two different docimemts
     Map<String, dynamic> dataToSend = {'timestamp': DateTime.now()};
     Map<String, dynamic> dataToSave = {'date': DateTime.now(), 'code': code};
 
-    // This part sends the timestamp of the scanned barcode and sends it to
-    // Firestore.
-    code = scanResult;
+    code = scanResult; // Assign the scanned result to the 'code' variable.
 
+    // Send the attendance timestamp to the student's own specific record in Firestore
     FirebaseFirestore.instance
         .collection('studentsWindermere')
         .doc(code)
         .collection('Attendance')
         .add(dataToSend);
 
+    // Save the timestamp to a different list, showing all the student's timestamp 
+    // together for that day
     FirebaseFirestore.instance
         .collection('studentsWindermere')
         .doc('all')
@@ -135,3 +160,4 @@ class _attendanceWindermereState extends State<attendanceWindermere> {
         .add(dataToSave);
   }
 }
+
