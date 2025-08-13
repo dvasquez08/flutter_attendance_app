535f266f1186434bc61cce1eb3e208e4c66049b8
David Vasquez <99619761+dvasquez08@users.noreply.github.com>
2025-08-12 22:34:29 -0600

    Updating comments to explain code base better

--- lib/West/testing_west.dart
+++ lib/West/testing_west.dart
@@ -1,19 +1,25 @@
+// Import for the other pages for the West location
 import 'package:attendance/West/attendance_west.dart';
 import 'package:attendance/West/ready_testing_west.dart';
+// Firebase imports
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+// Flutter imports
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+// Package imports that include Google fonts and the barcode scanner package
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:firebase_core/firebase_core.dart';
+// Importing the reuusable code from the components file
 import '../components.dart';
 
+// Initializing Firebase. This function is async because Firebase initialization must be completed before the app runs
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: testingWest()));
 }
-
+// The StatefulWidget that manages the check-in process for students attending testing at the West location.
 class testingWest extends StatefulWidget {
   const testingWest({Key? key}) : super(key: key);
 
@@ -22,15 +28,19 @@ class testingWest extends StatefulWidget {
 }
 
 class _testingWestState extends State<testingWest> {
+  // State variables to hold the testing attendance and if the form was submitted.
   String scanResult = " ";
   String code = " ";
   String paper = "received";
 
   @override
   Widget build(BuildContext context) {
+    // These variables capture the device's screen dimensions.
+    // They are used here for adjusting the size of containers and adding responsiveness.
     var heightDevice = MediaQuery.of(context).size.height;
     var widthDevice = MediaQuery.of(context).size.width;
     return Scaffold(
+      //App bar section that contains the screen title
       backgroundColor: Colors.transparent,
       appBar: AppBar(
         backgroundColor: Colors.black26,
@@ -38,6 +48,8 @@ class _testingWestState extends State<testingWest> {
             style: GoogleFonts.openSans(fontWeight: FontWeight.w300)),
         centerTitle: true,
       ),
+
+      // Main content of the screen, also sets the background image
       body: Container(
         alignment: Alignment.center,
         decoration: const BoxDecoration(
@@ -46,8 +58,11 @@ class _testingWestState extends State<testingWest> {
             fit: BoxFit.cover,
           ),
         ),
+
+        // Column arranges the content in a vertical array.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          // The page header, the logo, and instructions that directs the user to scan their barcode
           children: [
             Image.asset("assets/new-logo.png"),
             SizedBox(height: 15.0),
@@ -55,6 +70,8 @@ class _testingWestState extends State<testingWest> {
             SizedBox(height: 15.0),
             SansText("Scan your ID card below to checkin for testing", 25.0),
             SizedBox(height: 15.0),
+
+            // Button section for checking in students who attended testing
             GestureDetector(
               onTap: scanBarcode,
               child: Container(
@@ -71,6 +88,7 @@ class _testingWestState extends State<testingWest> {
                     ),
                   ],
                 ),
+                // The barcode logo placed inside the button
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -81,20 +99,24 @@ class _testingWestState extends State<testingWest> {
           ],
         ),
       ),
+
+      // Drawer for the slide-out menu for navigation.
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
+            // The header for the drawer.
             const DrawerHeader(
               decoration: BoxDecoration(color: Colors.red),
               child: SansText("Menu", 30.0),
             ),
+            // This column holds the navigation buttons inside the drawer.
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                NavButton("Class Attendance", attendanceWest()),
+                NavButton("Class Attendance", attendanceWest()), // Using the NavButton component, takes the user to the main attendance page for this location
                 SizedBox(height: 15.0),
-                NavButton("Ready for Testing", readyTestWest())
+                NavButton("Ready for Testing", readyTestWest()) // Using the the NavButton component, this takes the user back to the 'Ready for Testing' screen
               ],
             )
           ],
@@ -103,33 +125,38 @@ class _testingWestState extends State<testingWest> {
     );
   }
 
+  // Initiates the barcode scanning process and handles the result.
   Future scanBarcode() async {
     try {
       scanResult = await FlutterBarcodeScanner.scanBarcode(
-        "#ff6677",
-        "Cancel",
-        true,
-        ScanMode.BARCODE,
+        "#ff6677", // The color of the line across the scanner
+        "Cancel", // Shows the cancel button
+        true, // Boolean for enabling flash
+        ScanMode.BARCODE, // Specifying Barcode mode, QR sets to scan QR codes
       );
     } on PlatformException {
-      scanResult = "Failed to get platform version.";
+      scanResult = "Failed to get platform version."; // Error handling
     }
-    if (!mounted) return;
+    // A check to ensure the widget is still in the widget tree before updating state.
+    // This prevents errors if the user navigates away while scanning is in progress.
+    if (!mounted) return; 
 
-    setState(() => this.scanResult = scanResult);
+    setState(() => this.scanResult = scanResult); // Update the state with the result of the scan
 
-    // Telling the app what data to save
+    // Telling the app what data to send to Firestore which is indicating that the student
+    // submitted the form and attended testing
     Map<String, dynamic> dataToSend = {
       'Attended': DateTime.now(),
       'Paper': paper,
     };
 
     Map<String, dynamic> dataToSave = {
-      'timestamp': DateTime.now(),
+      'timestamp': DateTime.now(), // A different timestamp used if the student passed
     };
 
-    code = scanResult;
+    code = scanResult; // Assign the scanned result to the 'code' variable.
 
+    // Save the attendance timestamp to the specific student's record in Firestore.
     FirebaseFirestore.instance
         .collection('testing')
         .doc('west')
@@ -138,6 +165,7 @@ class _testingWestState extends State<testingWest> {
         .collection('info')
         .add(dataToSend);
 
+    // Send the timestamp if the student passed to their specific record in Firestore
     FirebaseFirestore.instance
         .collection('studentsWest')
         .doc(code)
@@ -145,3 +173,4 @@ class _testingWestState extends State<testingWest> {
         .add(dataToSave);
   }
 }
+
